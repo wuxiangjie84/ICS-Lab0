057f26752ec6d1c10b705c6a91e228e8a3f59ae9
wuxiangjie84 <wuxiangjie84@gmail.com>
2026-09-21 19:18:07 +0800

    Save resolved main.c

--- main.c
+++ main.c
@@ -3,9 +3,6 @@
 int main()
 {
     // @TODO: print a sentence you want.
-<<<<<<< HEAD
     printf("This message comes from main branch.\n");
-=======
     printf("This message comes from feature branch.\n");
->>>>>>> feature
 }
